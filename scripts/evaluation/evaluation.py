879ec849dd73ef0ab55c8da16fb98cbce5956cf1
Aline <alinelucia.m@gmail.com>
2025-04-01 10:41:36 +0200

    Some major refactoring and docs

--- scripts/evaluation/evaluation.py
+++ scripts/evaluation/evaluation.py
@@ -73,30 +73,38 @@ def gen_ground_truth(queries: pd.DataFrame, metadata: pd.DataFrame, ps_rel: int 
     return query_dict
 
 
-def DCG_score(relevance_scores: list, k: int) -> float:
+def dcg_score(relevance_scores: list[int], k: int) -> float:
+    """
+    For a list of relevance scores, compute the Discounted Cumulative Gain (DCG) at K.
+    DCG = sum^k_{i=1}(relevance_score_i / log2(rank_i + 1))
+    :param relevance_scores: list of relevance scores
+    :param k:
+    :return: DCG value
+    """
     topk = np.array(relevance_scores[:k])
     discounts = np.log2(np.arange(2, k + 2))
     return np.sum(topk / discounts)
 
-def nDCG_score(ground_truth, retrieved_list, k):
+
+def ndcg_score(ground_truth: list[str, float], retrieved_list: list[str, float], k: int) -> float:
     """
     Compute Normalized Discounted Cumulative Gain (nDCG) at K. By choosing ground truth and retrieved list, we can
     compute for different corpora.
     nDCG = DCG / iDCG
     (i)DCG = sum^k_{i=1}(relevance_score_i / log2(rank_i + 1))
-    :param ground_truth:
-    :param retrieved_list:
+    :param ground_truth: ground truth ranking with relevance scores
+    :param retrieved_list: graded ranking with relevance scores
     :param k:
     :return:
     """
     # get ideal DCG for ground truth
     log = np.log2(np.arange(2, k + 2))
-    iDCG = (ground_truth[:k] / log).sum()  # TODO: check slice is correct
-    dcg = (retrieved_list[:k] / log).sum()  # TODO: check slice is correct
-    return dcg / iDCG if iDCG > 0 else 0
+    idcg = (np.array(ground_truth[:k]) / log).sum()  # TODO: check slice is correct
+    dcg = (np.array(retrieved_list[:k]) / log).sum()  # TODO: check slice is correct
+    return dcg / idcg if idcg > 0 else 0
 
 
-def relativeD_score(ndcg_c1: float, ndcg_c2: float) -> float:
+def relative_d_score(ndcg_c1: float, ndcg_c2: float) -> float:
     """
     Compute relative delta from the paper. This is the difference between the nDCG scores of the two corpora.
     :param ndcg_c1: nDCG score for corpus 1
@@ -106,7 +114,7 @@ def relativeD_score(ndcg_c1: float, ndcg_c2: float) -> float:
     return 2 * (ndcg_c1 - ndcg_c2) / (ndcg_c1 + ndcg_c2) if (ndcg_c1 + ndcg_c2) > 0 else 0
 
 
-def evaluate_query(ground_truth, retrieved_list, k: [int], metadata: pd.DataFrame):
+def evaluate_query(ground_truth, retrieved_list, ks: [int], metadata: pd.DataFrame):
     """
     Compute the different nDCG@k scores for the different corpora. This function is for one query.
     :param ground_truth: 2D list of ground truth relevance scores - [image_path, relevance_score]
@@ -115,9 +123,8 @@ def evaluate_query(ground_truth, retrieved_list, k: [int], metadata: pd.DataFram
     :param metadata: Metadata dataframe for finding the original image path for altered images.
     :return: dictionary for nDCG and relative delta scores for each corpus
     """
-    # get ndcg for each k
     ndcg_scores = {}
-    for k in k:
+    for k in ks:
         # separate ranking list for different corpora
         ndcg_og = 0
         ndcg_c1 = 0
@@ -128,16 +135,17 @@ def evaluate_query(ground_truth, retrieved_list, k: [int], metadata: pd.DataFram
             'ndcg_c1': ndcg_c1,
             'ndcg_c2': ndcg_c2,
             'ndcg_c3': ndcg_c3,
-            'relD_og_c1': relativeD_score(ndcg_og, ndcg_c1),
-            'relD_og_c2': relativeD_score(ndcg_og, ndcg_c2),
-            'relD_og_c3': relativeD_score(ndcg_og, ndcg_c3),
-            'relD_c1_c2': relativeD_score(ndcg_c1, ndcg_c2),
-            'relD_c1_c3': relativeD_score(ndcg_c1, ndcg_c3),
-            'relD_c2_c3': relativeD_score(ndcg_c2, ndcg_c3),
+            'relD_og_c1': relative_d_score(ndcg_og, ndcg_c1),
+            'relD_og_c2': relative_d_score(ndcg_og, ndcg_c2),
+            'relD_og_c3': relative_d_score(ndcg_og, ndcg_c3),
+            'relD_c1_c2': relative_d_score(ndcg_c1, ndcg_c2),
+            'relD_c1_c3': relative_d_score(ndcg_c1, ndcg_c3),
+            'relD_c2_c3': relative_d_score(ndcg_c2, ndcg_c3),
         }
 
 
-def evaluate_all_queries(queries: [str], ground_truth, retrieved_list, k: [int], metadata: pd.DataFrame):
+def evaluate_all_queries(queries: [str], ground_truth: dict, retrieved_list: dict, ks: [int], metadata: pd.DataFrame) \
+        -> dict:
     """
     Compute the different nDCG@k scores for the different corpora. This function is for all queries.
     :param queries: list of queries
@@ -153,7 +161,7 @@ def evaluate_all_queries(queries: [str], ground_truth, retrieved_list, k: [int],
     # get average scores
     avg_scores = {}
     # ['ndcg_og', 'ndcg_c1', 'ndcg_c2', 'ndcg_c3', 'relD_og_c1', 'relD_og_c2', 'relD_og_c3', 'relD_c1_c2', 'relD_c1_c3', 'relD_c2_c3']
-    for k in k:
+    for k in ks:
         avg_scores[str(k)]({
             'ndcg_og': np.mean([s[str(k)]['ndcg_og'] for s in all_scores]),
             'ndcg_c1': np.mean([s[str(k)]['ndcg_c1'] for s in all_scores]),
@@ -166,3 +174,6 @@ def evaluate_all_queries(queries: [str], ground_truth, retrieved_list, k: [int],
             'relD_c1_c3': np.mean([s[str(k)]['relD_c1_c3'] for s in all_scores]),
             'relD_c2_c3': np.mean([s[str(k)]['relD_c2_c3'] for s in all_scores]),
         })
+    with open("evaluation_results.json", "w") as outfile:
+        json.dump(avg_scores, outfile)
+    return avg_scores
