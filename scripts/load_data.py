96ed83a2486547e6d3b8a0b4cc9c9a37a278b2a0
MV <62394699+Sub2mval@users.noreply.github.com>
2025-04-03 13:06:32 +0200

    Update load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -5,6 +5,7 @@ import clip
 import h5py
 import pandas as pd
 import torch
+import numpy as np
 
 
 def find(name, path):
@@ -23,7 +24,7 @@ def load_metadata(metadata_path: str) -> pd.DataFrame:
     return pd.read_csv(metadata_path)
 
 
-def load_images_and_generate_embeddings(imgs_path: str, metadata: pd.DataFrame, embeddings_path: str, embedding_model) -> dict:
+def load_images_and_generate_embeddings(imgs_path: str, metadata: pd.DataFrame, embeddings_path: str) -> dict:
     """
     Load the images, filter only the images we want to use based on metadata.
     Also generates embeddings for these images and saved them to a h5
@@ -36,53 +37,49 @@ def load_images_and_generate_embeddings(imgs_path: str, metadata: pd.DataFrame, 
     """
     # TODO load images and filter only the ones we want based on metadata
 
-    # TODO for
-    real_images = []
     fake_images = []
-    for i in range(len(metadata)):
-        if metadata['label'] == 'real':
-            real_images.append(metadata.iloc[i,0])
+    metallic = metadata[metadata['label'] == 'real']
+    real_images = np.asarray(metallic['image_id'])
+    og_img_paths = []
+    altered_img_paths = []
+    image_path = []
+
 
     for i in range(len(real_images)):
         meta = metadata[metadata['image_id'].str.contains(real_images[i])]
-        metal = meta['label' == 'fake']
-        fake_images.append(metal[i])
-        
-    linked_images = {
-        'original': real_images,
-        'fake': fake_images
-    }
+        real_images[i] = real_images[i] + '.jpg'
+        og_img_paths.append(find(real_images[i], imgs_path))
+        image_path.append(find(real_images[i], imgs_path))
+        alter1 = []
+        alter1path = []
+        metal = meta[meta['label'] == 'fake']
+        for j in metal['image_id']:
+            alter1.append(j + '.png')
+            alter1path.append(find(j + '.png', imgs_path))
+            image_path.append(find(j + '.png', imgs_path))
+        fake_images.append(alter1)
+        altered_img_paths.append(alter1path)
 
-    meta_linked = pd.dataframe(linked_images)
+    linked_images = [{'Real_image': x, 'Altered_Image': y} for x, y in zip(real_images, fake_images)]
+    linked_image_paths = [{'Real_image_path': x, 'Altered_Image_path': y} for x, y in zip(og_img_paths, altered_img_paths)]
+    #print(linked_images)
+    #print(linked_image_paths)
 
 
-    # TODO for
-    for i in range(len(meta_linked)):
-        meta_linked.iloc[i, 0] = meta_linked.iloc[i, 0] + '.png'
-        for k in range(len(meta_linked[i, 1])):
-            meta_linked.iloc[i, 1][k] = meta_linked.iloc[i, 1][k] + '.png'
-
-    og_img_paths = []
-    for i in range(len(meta_linked)):
-        img_path = find(meta_linked.iloc[i, 0], imgs_path)
-        og_img_paths.append(img_path)
-
-    altered_img_paths = []
-    for i in range(len(meta_linked)):
-        for k in range(len(meta_linked[i, 1])):
-            img_path = find(meta_linked.iloc[i, 1][k], imgs_path)
-            altered_img_paths.append(img_path)
-
-    image_path = og_img_paths + altered_img_paths
+    image_path[:] = [x for x in image_path if x]
+    print(image_path)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
     output_embeddings = []
     # generate embeddings for all sampled images
     for i in range(len(image_path)):
-        image = preprocess(Image.open(str(image_path[i])).unsqueeze(0).to(device))
-        with torch.no_grad():
-            image_features = embedding_model.encode_image(image)
-            output_embeddings.append(image_features)
+        if image_path[i] != '[None]':
+            image = preprocess(Image.open(str(image_path[i]))).unsqueeze(0).to(device)
+            with torch.no_grad():
+                image_features = model.encode_image(image)
+                #print(image_features)
+                output_embeddings.append(image_features)
+    #print(output_embeddings)
 
     # Save embeddings to h5 file
     with h5py.File(embeddings_path, 'w') as f:
@@ -134,3 +131,18 @@ def load_queries_and_image_ids(query_path: str):
         image_list.update(altered)
 
     return query_df, list(image_list)
+
+# To test
+# QUERY_PATH = '../data/queries_at_least_3_sufficient_altered.csv'
+# query_df, image_list = load_queries_and_image_ids(QUERY_PATH)
+# print(query_df)
+# print(image_list)
+
+if __name__ == '__main__':
+    METADATA_PATH = '../metadata/metadata_OpenImages.csv'
+    PATH_TO_EMBEDDINGS = '../data/embeddings/img_embeddings.h5py'
+    IMG_PATH = '../data_openImages'  # TODO define this Mohit
+    QUERY_PATH = '../data/queries_at_least_3_sufficient_altered.csv'
+    RESULT_PATH = '../results/' 
+    metadata = pd.read_csv(METADATA_PATH)
+    load_images_and_generate_embeddings(IMG_PATH, metadata=metadata, embeddings_path=RESULT_PATH)
